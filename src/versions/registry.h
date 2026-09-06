999c33a972b1bb03ab2ad249ff9122145507e8a5
bicarus <202771338+bicarus-dev@users.noreply.github.com>
2026-09-06 15:16:17 -0700

    add LDJ-6a276d7c_b0397c

--- src/versions/registry.h
+++ src/versions/registry.h
@@ -2,9 +2,12 @@
 
 #include "LDJ-68a2bd1c_b1c7bc.h"
 #include "LDJ-69ddf5f8_af6cdc.h"
+#include "LDJ-6a276d7c_b0397c.h"
 
 namespace iidxfreq::versions {
 
-inline constexpr std::array profiles{&ldj_68a2bd1c_b1c7bc, &ldj_69ddf5f8_af6cdc};
-
+inline constexpr std::array profiles{
+    &ldj_68a2bd1c_b1c7bc,
+    &ldj_69ddf5f8_af6cdc,
+    &ldj_6a276d7c_b0397c};
 }
